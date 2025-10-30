6c4dbe0cf9356066bfaea89fcd50acef7aa5d2ac
Natasa15-aaa <akazadctgdk@gmail.com>
2025-10-30 12:19:23 +0600

    Add files via upload

---
+++ Sheet1(R).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+int N;
+cin>>N;
+int years = N/365;
+int rem = N%365;
+int months = rem/30;
+int  days = rem%30;
+cout<<years<<"years"<<endl;
+cout<<months<<"months"<<endl;
+cout<<days<<"days"<<endl;
+return 0;
+}
+
